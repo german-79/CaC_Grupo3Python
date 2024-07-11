c7164e2bcfb8e52ba89849a03e5001912362a574
German <german.oliverapere@gmail.com>
2024-07-10 21:43:24 -0300

    login mas

--- Back/app.py
+++ Back/app.py
@@ -126,3 +126,45 @@ def borrar(id):
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+#agrego German
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import os
+
+app = Flask(__name__, template_folder='Front/templates')
+app.secret_key = os.urandom(15)  # clave aleatoria segura
+
+# Usuarios de prueba (nombre de usuario: contraseña)
+users = {
+    'prueba1': '1234',
+    'prueba2': '4321'
+}
+
+@app.route('/')
+def home():
+    if 'username' in session:
+        return render_template('index_intranet.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            flash('Credenciales inválidas')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
